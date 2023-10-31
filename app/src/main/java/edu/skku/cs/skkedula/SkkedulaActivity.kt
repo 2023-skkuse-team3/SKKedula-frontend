f94ab10636148048dd1d2cb25f810880a70650e7
ggyuchive <zarami1214@g.skku.edu>
2023-11-01 02:45:42 +0900

    [ADD] Title Bar & Logout button

--- app/src/main/java/edu/skku/cs/skkedula/SkkedulaActivity.kt
+++ app/src/main/java/edu/skku/cs/skkedula/SkkedulaActivity.kt
@@ -2,6 +2,8 @@ package edu.skku.cs.skkedula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -17,8 +19,16 @@ class SkkedulaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skkedula)
+
+        // apply navigation bar
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
         findViewById<BottomNavigationView>(R.id.nav_view).setupWithNavController(navController)
+
+        // TODO: Add action when logout button pressed
+        val logout = findViewById<ImageView>(R.id.imageView)
+        logout.setOnClickListener{
+            Toast.makeText(this,"로그아웃 버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
     }
 }
