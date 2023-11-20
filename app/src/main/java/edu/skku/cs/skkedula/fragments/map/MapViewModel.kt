e7c4f41097cc0d447afa15b596b21097f3fc044f
silverlif <yesom.ha@gmail.com>
2023-11-20 16:47:40 +0900

    revised

--- app/src/main/java/edu/skku/cs/skkedula/fragments/map/MapViewModel.kt
+++ app/src/main/java/edu/skku/cs/skkedula/fragments/map/MapViewModel.kt
@@ -11,11 +11,11 @@ class MapViewModel : ViewModel() {
     val startLocation = MutableLiveData<String>()
     val endLocation = MutableLiveData<String>()
 
-    // Add a new LiveData for marker click
+    /*// Add a new LiveData for marker click
     private val _markerClick = MutableLiveData<Study?>()
     val markerClicked: LiveData<Study?> = _markerClick
 
     fun onMarkerClicked(entrance: Study?) {
         _markerClick.value = entrance
-    }
+    }*/
 }
