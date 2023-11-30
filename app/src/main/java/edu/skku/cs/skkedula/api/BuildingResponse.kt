5976042b01ba4a494cc906b788095dd7be7650d7
silverlif <yesom.ha@gmail.com>
2023-11-30 21:34:18 +0900

    map final

--- app/src/main/java/edu/skku/cs/skkedula/api/BuildingResponse.kt
+++ app/src/main/java/edu/skku/cs/skkedula/api/BuildingResponse.kt
@@ -3,10 +3,10 @@ package edu.skku.cs.skkedula.api
 import com.google.gson.annotations.SerializedName
 
 data class BuildingResponse(
-    @SerializedName("Building_num")
-    val buildingName: String = "", // nullable 제거
+    @SerializedName("Building_name")
+    val buildingName: String,
     @SerializedName("Latitude")
-    val latitude: Double = 0.0, // Double로 변경
+    val latitude: Double,
     @SerializedName("Longitude")
-    val longitude: Double = 0.0 // Double로 변경
+    val longitude: Double
 )
